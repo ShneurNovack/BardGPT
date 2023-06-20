9a90aca5d37235ad94504c69d95779278798d803
ShneurNovack <81717928+ShneurNovack@users.noreply.github.com>
2023-06-20 03:00:07 -0400

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -1,1 +1,1 @@
-export const BACKEND_API_URL = "https://gptbard.reshapecreative.com:5000";
+export const BACKEND_API_URL = "https://shneurcors.herokuapp.com/http://gptbard.reshapecreative.com:5000";
